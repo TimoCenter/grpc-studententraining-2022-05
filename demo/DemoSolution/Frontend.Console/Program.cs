eca7dbf0457f544ee82ec595506002926944bd84
JP <jp@jp.nl>
2022-05-23 17:04:50 +0200

    Console-client spreekt backend aan

---
+++ demo/DemoSolution/Frontend.Console/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Frontend.Console.Protos;
+using Grpc.Net.Client;
+
+Console.WriteLine("Hello, World!");
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7284");
+var client = new CarService.CarServiceClient(channel);
+
+var request = new GetAllRequest();
+var reply = await client.GetAllAsync(request);
+
+foreach (var car in reply.Cars)
+{
+    Console.WriteLine($"{car.Make} {car.Model} heeft {car.Kilometers}km op de teller");
+}
